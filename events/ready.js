3d93c5c73d77197a075a737fcd7f94cc54040289
jerinxzone <45122287+jerinxzone@users.noreply.github.com>
2018-11-17 21:26:02 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,10 @@
 const config = require('../config.json');
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
 exports.run = (client) => {
     console.log(`[Radio-Streaming-Bot] Bot ready in ${client.guilds.size} server(s).`);
     console.log(`[Radio-Streaming-Bot] Playing with ${client.users.size} users.`);
     console.log(`[Radio-Streaming-Bot] Bot Invite: https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`);
+	bot.user.setActivity("Playing Radio Indonesia GaneshaFM");
   }
