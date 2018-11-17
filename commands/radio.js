827556ad6047fda1c6a65a5bb745a0f82e540e1c
jerinxzone <45122287+jerinxzone@users.noreply.github.com>
2018-11-17 20:47:21 +0700

    Update radio.js

--- commands/radio.js
+++ commands/radio.js
@@ -2,6 +2,6 @@ const config = require('../config.json');
 
 exports.run = (client, message, args) => {
 
-return message.reply(`\n${config.prefix}Radio - Daftar Radio\nprambors\ngen\ngensby\nwijaya\ngiri\nswaragama\nhardrock\nindika\nmendhut\npatria\n\nCara Play - !r <nama radio>`)
+return message.reply(`\n${config.prefix}Radio - Daftar Radio\nprambors\ngen\ngensby\nwijaya\ngiri\nswaragama\nhardrock\nindika\nmendhut\npatria\nwijangsongko\n\nCara Play - !r <nama radio>`)
 
 }
