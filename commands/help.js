46be188ffe5588ca2de9e9952c8f4524b6c7ef53
jerinxzone <45122287+jerinxzone@users.noreply.github.com>
2018-11-17 19:27:26 +0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+const config = require('../config.json');
+
+exports.run = (client, message, args) => {
+
+return message.reply(`\n${config.prefix}Help - Displays Help Menu\n${config.prefix}Radio - DAFTAR RADIO\n${config.prefix}r <nama radio> - Joins And Plays A Radio Stream\n${config.prefix}Leave - Leaves Voice Channel.\n${config.prefix}Prefix <New Prefix> - Sets New Prefix. **[BOT OWNER ONLY${config.prefix}]**`)
+
+}
