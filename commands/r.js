0bbb001634ef6e575c1ac0602d61d79c71513687
jerinxzone <45122287+jerinxzone@users.noreply.github.com>
2018-11-19 13:40:47 +0700

    Update r.js

--- commands/r.js
+++ commands/r.js
@@ -9,8 +9,8 @@ exports.run = (client, message, args) => {
 	
     const prambors = "http://masima.rastream.com/masima-pramborsjakarta?";
 	const patria = "http://www.radiopatria.net:8000/stream";
-	const gen = "https://onlineradiobox.com/id/genjakarta/?cs=id.genjakarta&played=1";
-	const gensbyradio = "https://onlineradiobox.com/id/gensurabaya/?cs=id.gensurabaya&played=1";
+	const gen = "https://mytuner-radio.com/radio/gen-fm-987-416443/";
+	const gensbyradio = "https://mytuner-radio.com/radio/1031-gen-fm-surabaya-416595/";
 	const wijaya = "http://wijayafm.onlivestreaming.net:9880/live";
 	const giri = "http://streaming.girifm.com:8010/;";
 	const swaragama = "http://jkt.jogjastreamers.com:8000/swaragama456789stereo";
